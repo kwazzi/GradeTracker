04578c0bcfbec2a88026a6e1244e53b45aacacd7
kwazzi <54319675+kwazzi@users.noreply.github.com>
2020-01-13 08:46:12 -0600

    updated

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,13 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("TrackerGui.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 500, 500));
-        primaryStage.show();
+        Visualizer visualizer = new Visualizer(primaryStage);
     }
 
 
