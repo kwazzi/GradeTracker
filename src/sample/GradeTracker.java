9d2e480b8171865157b49ac8fa9d7fe2fb5bc0cb
kwazzi <54319675+kwazzi@users.noreply.github.com>
2020-01-13 08:47:31 -0600

    Update GradeTracker.java

--- src/sample/GradeTracker.java
+++ src/sample/GradeTracker.java
@@ -1,4 +1,20 @@
 package sample;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class GradeTracker {
+    ArrayList<Course> courses = new ArrayList<>();
+
+    public GradeTracker(){
+
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<Course> courses) {
+        this.courses = courses;
+    }
 }
